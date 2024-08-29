7313f0c9416a70ed7957d2bc261d99af2ebe43ce
hayat118 <tariqueht6@gmail.com>
2024-08-29 17:48:59 +0530

    wip in profile page

--- app/business/add-business.jsx
+++ app/business/add-business.jsx
@@ -2,16 +2,29 @@ import { View, Text, Image, TouchableOpacity, TextInput } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "expo-router";
 import * as ImagePicker from "expo-image-picker";
+import RNPickerSelect from "react-native-picker-select";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../../configs/FirebaseConfig";
+import { Colors } from "../../constants/Colors";
 
 const addBusiness = () => {
   const navigation = useNavigation();
   const [image, setImage] = useState(null);
+  const [categoryList, setCategoryList] = useState([]);
+  const [name, setName] = useState();
+  const [address, setAddress] = useState();
+  const [contact, setContact] = useState();
+  const [website, setWebsite] = useState();
+  const [about, setAbout] = useState();
+  const [category, setCategory] = useState();
 
+  //
   useEffect(() => {
     navigation.setOptions({
       headerTitle: "Add New Business",
       headerShown: true,
     });
+    GetCategoryList();
   }, []);
 
   const onImagePick = async () => {
@@ -23,7 +36,27 @@ const addBusiness = () => {
       quality: 1,
     });
     setImage(result?.assets[0].uri);
-    // console.log(result, "re");
+  };
+
+  const GetCategoryList = async () => {
+    setCategoryList([]);
+    try {
+      const q = query(collection(db, "Category"));
+      const snapShot = await getDocs(q);
+
+      snapShot.forEach((doc) => {
+        // console.log(doc.data());
+        setCategoryList((prev) => [
+          ...prev,
+          {
+            label: doc.data().name,
+            value: doc.data().name,
+          },
+        ]);
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -60,7 +93,6 @@ const addBusiness = () => {
             padding: 10,
             backgroundColor: "white",
             width: 70,
-            // height: 70,
             borderRadius: 10,
           }}
         >
@@ -84,18 +116,110 @@ const addBusiness = () => {
           )}
         </View>
       </TouchableOpacity>
-      <View style={{ marginTop: 10 }}>
+      <View>
         <TextInput
           placeholder="Name"
+          onChangeText={(value) => setName(value)}
           style={{
             padding: 10,
             borderRadius: 5,
             borderWidth: 1,
             fontSize: 15,
             backgroundColor: "white",
+            marginTop: 10,
+            borderColor: Colors.Primary,
+          }}
+        />
+        <TextInput
+          placeholder="Address"
+          onChangeText={(value) => setAddress(value)}
+          style={{
+            padding: 10,
+            borderRadius: 5,
+            borderWidth: 1,
+            fontSize: 15,
+            backgroundColor: "white",
+            marginTop: 10,
+            borderColor: Colors.Primary,
+          }}
+        />
+        <TextInput
+          placeholder="Contact"
+          onChangeText={(value) => setContact(value)}
+          style={{
+            padding: 10,
+            borderRadius: 5,
+            borderWidth: 1,
+            fontSize: 15,
+            backgroundColor: "white",
+            marginTop: 10,
+            borderColor: Colors.Primary,
+          }}
+        />
+        <TextInput
+          placeholder="Website"
+          onChangeText={(value) => setWebsite(value)}
+          style={{
+            padding: 10,
+            borderRadius: 5,
+            borderWidth: 1,
+            fontSize: 15,
+            backgroundColor: "white",
+            marginTop: 10,
+            borderColor: Colors.Primary,
+          }}
+        />
+        <TextInput
+          multiline={true}
+          numberOfLines={3}
+          placeholder="About"
+          onChangeText={(value) => setAbout(value)}
+          style={{
+            padding: 10,
+            borderRadius: 5,
+            borderWidth: 1,
+            fontSize: 15,
+            backgroundColor: "white",
+            marginTop: 10,
+            height: 100,
+            borderColor: Colors.Primary,
           }}
         />
       </View>
+      <View
+        style={{
+          borderRadius: 5,
+          borderWidth: 1,
+          fontSize: 15,
+          backgroundColor: "white",
+          marginTop: 10,
+          borderColor: Colors.Primary,
+        }}
+      >
+        <RNPickerSelect
+          onValueChange={(value) => setCategory(value)}
+          items={categoryList}
+        />
+      </View>
+      <TouchableOpacity
+        style={{
+          padding: 15,
+          backgroundColor: Colors.Primary,
+          borderRadius: 5,
+          marginTop: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "outfit",
+            fontSize: 16,
+            textAlign: "center",
+            color: "white",
+          }}
+        >
+          Add New Business
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
--- configs/FirebaseConfig.jsx
+++ configs/FirebaseConfig.jsx
@@ -2,6 +2,8 @@
 // import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,4 +22,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 // const analytics = getAnalytics(app);
